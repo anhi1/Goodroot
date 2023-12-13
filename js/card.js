e6b2c3892da844679103dffc0543ab8147256fab
anhi1 <anhiemolina@gmail.com>
2023-12-13 18:39:54 +0100

    PrintModal

--- js/card.js
+++ js/card.js
@@ -1,48 +1,86 @@
 const searchBox = document.querySelector(".buscador input");
 const searchBtn = document.querySelector(".buscador button");
-const plantContainer = document.getElementById('plants');
 let allPlants = [];  // Almacenar todas las plantas originales
 
-// Función para imprimir las plantas en el contenedor
-function printPlants(data) {
-    plantContainer.innerHTML = ''; // Limpiar el contenido existente antes de agregar nuevos elementos
+function loadData(x) {
+    // fetch('https://cristinamaser.com/api/invitations/'+parseInt(x)+1)
+    // .then(response => response.json())
+    // .then(responseJson => printModal(responseJson))
+    // x = parseInt(x) + 1;
+    fetch('plants.json')
+    .then(response => response.json())
+    .then(responseJson => printModal(responseJson.plants[x]))
+  }
 
+function printModal(x) { 
+    console.log(x)
+    let miModal = new bootstrap.Modal(document.getElementById('infoModal'));
+    miModal.show();
+    let modalTitleElement = miModal._element.querySelector('.modal-title');
+    let modalBodyElement = miModal._element.querySelector('.modal-body');
+    modalTitleElement.innerHTML = x.name
+    modalBodyElement.innerHTML = 
+    `<div>
+    <img src="${x.photo}" class="card-img-top" alt="plantas">
+    <p>${x.description}</p>
+    </div>`
+    document.getElementById('closeModalButton').addEventListener('click', function(){
+        miModal.hide();
+      });
+}
+
+
+
+
+function printPlants(data){
+    let plantContainer = document.getElementById('plants');
+    plantContainer.innerHTML = ''; // Limpiar el contenido existente antes de agregar nuevos elementos
     for (let i in data) {
-        plantContainer.innerHTML += 
-        `<div class="card m-4" style="width:280px;">
-            <a class="descripcion" href="description.html"><img src="${data[i].photo}" class="card-img-top" alt="plantas"></a>  
+        plantContainer.innerHTML +=
+            `<div class="card m-4" style="width:280px;">
+            <a class="modal-dialog" href=""><img src="${data[i].photo}" class="card-img-top" alt="plantas"></a>  
             <div class="card-body">
                 <span class="card-text"><strong>${data[i].name}</strong></span>
                 <p class="card-text"><strong>Precio:</strong> ${data[i].price}€</p>
                 <p class="card-text"><strong>Temperatura:</strong> ${data[i].temperature}°C</p>
                 <p class="card-text"><strong>Riego:</strong> ${data[i].riego}</p>
-            </div>   
+            </div>
+
+            <a data-value="${i}" class="btn btn-primary activeModal" href="#">Ver</a>
         </div>`;
     }
+    let linkModales = document.getElementsByClassName('activeModal');
+    for(let i in linkModales){
+        linkModales[i].addEventListener('click', function(e){
+            e.preventDefault();
+            loadData(e.target.dataset.value)
+        })
+    }
 }
+fetch('plants.json')
+    .then(res => res.json())
+    .then(datosRecibidosJson => printPlants(datosRecibidosJson.data))
 
-// Evento de clic en el botón de búsqueda
+
+
+
+
+
 searchBtn.addEventListener("click", () => {
-    // Obtener el término de búsqueda y convertirlo a minúsculas
     const searchTerm = searchBox.value.toLowerCase();
 
-    // Filtrar las plantas que coinciden con el término de búsqueda
     const filteredPlants = allPlants.filter(plant =>
         plant.name.toLowerCase().includes(searchTerm)
     );
+    printPlants(filteredPlants);   // Imprimir las plantas filtradas
 
-    // Imprimir las plantas filtradas
-    printPlants(filteredPlants);
 });
-
 // Realizar la solicitud fetch para obtener todos los datos
 fetch('plants.json')
     .then(res => res.json())
     .then(datosRecibidosJson => {
-        // Almacenar todas las plantas originales
         allPlants = datosRecibidosJson.plants;
-        // Imprimir todas las plantas
         printPlants(allPlants);
-    })
-    .catch(error => console.error("Error al obtener datos del servidor:", error));
+    }) 
+
 
